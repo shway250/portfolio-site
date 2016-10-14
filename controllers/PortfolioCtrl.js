0733429886a92c073f353e87d1acc042554e5760
shway250 <jackson_duhon@yahoo.com>
2016-10-13 17:20:15 -0700

    added all project photos

--- controllers/PortfolioCtrl.js
+++ controllers/PortfolioCtrl.js
@@ -1,10 +1,14 @@
 jacksonSite.controller('PortfolioCtrl', ['$scope', function($scope) {
+  //links
   $scope.accioBikeLink = 'https://accio-bike.herokuapp.com/';
   $scope.wubbaLubbaLink = 'https://shway250.github.io/Rick-Morty-Four-Square/';
   $scope.artemisLink = 'http://artemistracker.herokuapp.com/';
   $scope.euclidLink = 'https://miguelapou.github.io/Euclid/';
-
-
+  //images
+  $scope.wubbaLubbaImg = '../img/wubba-lubba.png';
+  $scope.accioBikeImg = '../img/accio-bike.png';
+  $scope.artemisImg = '../img/artemis.png';
+  $scope.euclidImg = '../img/euclid.png';
 
   $scope.scroll = function() {
     var paths = ['/', '/about', '/portfolio', 'experience', 'skills'];
