17f2544b848cb8126b163906d8e40bb73bdaa3ed
shway250 <jackson_duhon@yahoo.com>
2016-10-14 15:03:55 -0700

    portfolio page lookin better

--- controllers/PortfolioCtrl.js
+++ controllers/PortfolioCtrl.js
@@ -9,6 +9,12 @@ jacksonSite.controller('PortfolioCtrl', ['$scope', function($scope) {
   $scope.accioBikeImg = '../img/accio-bike.png';
   $scope.artemisImg = '../img/artemis.png';
   $scope.euclidImg = '../img/euclid.png';
+  //descriptions
+  $scope.wubbaLubbaDesc = 'A front end Connect Four game based off the cartoon "Rick and Morty". Technologies used include HTML, CSS, Javascript, jQuery, GitHub.'
+  $scope.accioBikeDesc = 'A full stack web application for storing information about your bike and web crawling through Craigslist to help find it if it is stolen. Technologies used include HTML, CSS, Javascritp, Ruby on Rails, NokoGiri, GitHub.'
+  $scope.artemisDesc = 'A full stack web application that helps you keep track of all the applications you’re sending during a job search. Technologies used include MongoDb, Express, Angular.js, Node.js. Won first place in the GA MEAN stack Hackathon'
+  $scope.euclidDesc = 'A front end FM Synthesizer. Allows the user to create generative compositions using just the browser. Technologies used include HTML, CSS, Javascript, Canvas, P5.js.'
+  
 
   $scope.scroll = function() {
     var paths = ['/', '/about', '/portfolio', 'experience', 'skills'];
