8f8b13d3630a9996609f90ac02c87ffc9eb2343a
shway250 <jackson_duhon@yahoo.com>
2016-10-19 15:42:12 -0700

    trying different things in about controller and main controller to see if I can load img

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 jacksonSite.controller('MainCtrl', ['$scope', function($scope) {
   $scope.mainText = 'I am a full stack web developer with a passion for music technology and digital media. In my work I seek to unify aesthetic design with exacting functionality.';
-  $scope.mainImg = '../img/portfolio_profile_pick.jpg';
+  $scope.mainImg = '../portfolio-site/img/portfolio_profile_pick.jpg';
 
   $scope.scroll = function() {
     $scope.paths = ['/', '/about', '/portfolio', 'experience', 'skills'];
